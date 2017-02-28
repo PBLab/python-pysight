5e741599ac02b617ae0b448e23fddf75fa6cc070
Hagai Hargil <hagaihargil@gmail.com>
2017-02-28 10:50:58 +0200

    No requirements specified, making sure RTD is OK and status of PyPI

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'PySight'
 year = '2017'
 author = 'Hagai Hargil'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.2'
+version = release = '0.1.3'
 
 pygments_style = 'trac'
 templates_path = ['.']
