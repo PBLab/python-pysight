d7ca73f1c428653dffbf518ae7a891f6ff6d5c41
Hagai Hargil <hagaihargil@gmail.com>
2017-05-31 20:28:40 +0300

    Minor tweaks for version update

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'PySight'
 year = '2017'
 author = 'Hagai Hargil'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.4.7'
+version = release = '0.4.8'
 autodoc = True
 
 pygments_style = 'trac'
