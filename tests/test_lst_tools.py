215e57671a0b6b4aae303af540450ea3382f1b9d
Hagai Hargil <hagaihargil@gmail.com>
2018-02-21 18:23:54 +0200

    More changes

--- tests/test_lst_tools.py
+++ tests/test_lst_tools.py
@@ -6,22 +6,22 @@ from pysight.ascii_list_file_parser.fileIO_tools import FileIO
 from pysight.ascii_list_file_parser import timepatch_switch
 
 
-class TestAllocationTools(unittest.TestCase):
-    length = 100
-    df = pd.DataFrame([i for i in range(length)], columns=['abs_time'])
-    dict_of_data = {
-        'PMT1': pd.DataFrame([[1, 2, 3, 4, 5, 6, 7], [5, 6, 7, 8, 9, 10, 11]],
-                             columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep',
-                                      'time_rel_pulse', 'bins_x', 'bins_y'], dtype=np.uint64),
-        'Lines': pd.DataFrame([[1, 2, 3, 4], [5, 6, 7, 8]],
-                              columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep']),
-        'Laser': pd.DataFrame([[0, 2, 3, 2], [3, 6, 7, 6]],
-                              columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep'])
-    }
-    dict_of_data['PMT1'].time_rel_pulse = dict_of_data['PMT1'].time_rel_pulse.astype(np.uint8)
-    cur_file = FileIO(filename='tests' + sep + 'tests_data' + sep + 'data_for_tests_1.lst')
-    cur_file.run()
-    dict_of_slices_hex = timepatch_switch.ChoiceManagerHex().process(cur_file.timepatch)
+# class TestAllocationTools(unittest.TestCase):
+#     length = 100
+#     df = pd.DataFrame([i for i in range(length)], columns=['abs_time'])
+#     dict_of_data = {
+#         'PMT1': pd.DataFrame([[1, 2, 3, 4, 5, 6, 7], [5, 6, 7, 8, 9, 10, 11]],
+#                              columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep',
+#                                       'time_rel_pulse', 'bins_x', 'bins_y'], dtype=np.uint64),
+#         'Lines': pd.DataFrame([[1, 2, 3, 4], [5, 6, 7, 8]],
+#                               columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep']),
+#         'Laser': pd.DataFrame([[0, 2, 3, 2], [3, 6, 7, 6]],
+#                               columns=['abs_time', 'edge', 'sweep', 'time_rel_sweep'])
+#     }
+#     dict_of_data['PMT1'].time_rel_pulse = dict_of_data['PMT1'].time_rel_pulse.astype(np.uint8)
+#     cur_file = FileIO(filename='tests' + sep + 'tests_data' + sep + 'data_for_tests_1.lst')
+#     cur_file.run()
+#     dict_of_slices_hex = timepatch_switch.ChoiceManagerHex().process(cur_file.timepatch)
 
     # analyzed = Allocate(dict_of_data=cur_file.dict_of_input_channels)
     #
