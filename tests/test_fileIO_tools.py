303ca9f94fbc94930786635a4c98aefa29b6611a
Hagai Hargil <hagaihargil@gmail.com>
2018-02-21 19:37:19 +0200

    Appveyor fixes

--- tests/test_fileIO_tools.py
+++ tests/test_fileIO_tools.py
@@ -9,8 +9,8 @@ import pathlib
 class TestFileIOTools(unittest.TestCase):
     """ Tests for new multiscaler readout functions """
 
-    list_of_file_names = [str(pathlib.Path(r'./tests_data/1.lst').absolute()),
-                          str(pathlib.Path(r'./tests_data/2.lst').absolute())]
+    list_of_file_names = [str(pathlib.Path(r'tests/tests_data/1.lst').absolute()),
+                          str(pathlib.Path(r'tests/tests_data/2.lst').absolute())]
     file_io_objects = []
     for file in list_of_file_names:
         cur_obj = FileIO(file, debug=False, input_start='Frames',
