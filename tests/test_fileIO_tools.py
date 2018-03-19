a9e64f29466cb404f9da6a3484238bed3f6a94d1
HagaiHargil <hagaihargil@gmail.com>
2018-03-19 22:25:58 +0200

    added crlf to all files

--- tests/test_fileIO_tools.py
+++ tests/test_fileIO_tools.py
@@ -44,7 +44,7 @@ class TestFileIOTools(unittest.TestCase):
         self.assertEqual(list_of_real_time_patch, list_of_returned_time_patch)
 
     def test_check_start_of_data_value(self):
-        list_of_real_start_loc = [1647, 1477]
+        list_of_real_start_loc = [1749, 1567]
         list_of_returned_locs = []
         for oname in self.file_io_objects:
             list_of_returned_locs.append(oname.start_of_data_pos)
