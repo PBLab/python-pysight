51ab9da4c5a2a35545c0f0806c83df761d7bf2a0
HagaiHargil <hagaihargil@gmail.com>
2018-05-21 21:50:09 +0300

    Almost working

--- src/pysight/binary_list_file_parser/binary_parser.py
+++ src/pysight/binary_list_file_parser/binary_parser.py
@@ -1,22 +1,29 @@
 from enum import Enum
-from typing import Tuple
+from typing import NamedTuple
 import warnings
 
 import numpy as np
 import attr
 from attr.validators import instance_of
 
+class TimepatchBits(NamedTuple):
+    total: int
+    time: int
+    sweep: int
+    tag: int
+    lost: int
+
 
 class Timepatch(Enum):
-    Tp0 = '0',
-    Tp5 = '5',
-    Tp1 = '1',
-    # Tp5b =
-    # TpDb =
-    # Tpf3 =
-    # Tp43 =
-    # Tpc3 =
-    # Tp3 =
+    Tp0 = TimepatchBits(16, 12, 0, 0, 0)
+    Tp5 = TimepatchBits(32, 20, 8, 0, 0)
+    Tp1 = TimepatchBits(32, 28, 0, 0, 0)
+    Tp5b = TimepatchBits(64, 28, 16, 15, 1)
+    TpDb = TimepatchBits(64, 28, 16, 16, 0)
+    Tpf3 = TimepatchBits(64, 36, 7, 16, 1)
+    Tp43 = TimepatchBits(64, 44, 0, 15, 1)
+    Tpc3 = TimepatchBits(64, 44, 0, 16, 0)
+    Tp3 = TimepatchBits(64, 54, 0, 6, 1)
 
 
 @attr.s
@@ -32,11 +39,34 @@ class BinaryDataParser:
     sweep = attr.ib(init=False)
     lost = attr.ib(init=False)
     dict_of_data = attr.ib(init=False)
+    timepatch_bits = attr.ib(init=False)
+
+    def __attrs_post_init__(self):
+        tpdict = {'0': Timepatch.Tp0,
+                  '5': Timepatch.Tp5,
+                  '1': Timepatch.Tp1,
+                  '5b': Timepatch.Tp5b,
+                  }
+        try:
+            self.timepatch_bits = tpdict[self.timepatch]
+        except KeyError:
+            raise KeyError(f"Invalid timepatch value received: {self.timepatch}.")
 
     def run(self):
         """ Main pipeline for the parsing """
         self.channel = self.__get_channel()
         self.edge = self.__get_edge()
+        self.time = self.__get_time()
+        if self.timepatch_bits.value.sweep != 0:
+            self.sweep = self.__get_sweep()
+        if self.timepatch != 'f3':
+            if self.timepatch_bits.value.tag != 0:
+                self.tag = self.__get_tag()
+            if self.timepatch_bits.value.lost != 0:
+                self.lost = self.__get_lost()
+        else:
+            self.tag = self.__get_tag_f3()
+            self.lost = self.__get_lost_f3()
 
 
     def __get_channel(self) -> np.ndarray:
@@ -61,4 +91,49 @@ class BinaryDataParser:
         """
         return (np.right_shift(self.data, 3) & 1).astype(np.uint8)
 
+    def __get_time(self) -> np.ndarray:
+        """
+        Parse the time bits.
+        Return:
+        -------
+            :param time np.ndarray: Array of the absolute times for each event.
+        """
+        ones = int('1' * self.timepatch_bits.value.time, 2)  # time bits
+        time = np.right_shift(self.data, 4) & ones
+        return time.astype(np.uint64)
 
+    def __get_sweep(self):
+        """
+        Parse the sweep bits.
+        Return:
+        -------
+            :param sweep np.ndarray: Array of the sweep number for each event.
+        """
+        ones = int('1' * self.timepatch_bits.value.sweep, 2)
+        right_shift_by = 4 + self.timepatch_bits.value.time
+        sweep = np.right_shift(self.data, right_shift_by) & ones
+        return sweep.astype(np.uint16)
+
+    def __get_tag(self):
+        """
+        Parse the tag bits.
+        Return:
+        -------
+            :param tag np.ndarray: Array of the tag bits for each event.
+        """
+        ones = int('1' * self.timepatch_bits.value.tag, 2)
+        right_shift_by = 4 + self.timepatch_bits.value.time + \
+            self.timepatch_bits.value.sweep
+        tag = np.right_shift(self.data, right_shift_by) & ones
+        return tag.astype(np.uint16)
+
+    def __get_lost(self):
+        """
+        Parse the lost bits.
+        Return:
+        -------
+            :param lost np.ndarray: Array of the lost bit for each event.
+        """
+        right_shift_by = self.timepatch_bits.value.total - 1
+        lost = np.right_shift(self.data, right_shift_by) & 1
+        return lost.astype(np.uint8)
--- tests/test_binary_parse.py
+++ tests/test_binary_parse.py
@@ -18,3 +18,60 @@ class BinaryTest(TestCase):
         edge = self.bindata._BinaryDataParser__get_edge()
         np.testing.assert_array_equal(edge, np.array([0, 0, 1, 0, 1],
                                                      dtype=np.uint8))
+
+    def test_time_with_tp0(self):
+        timepatch = '0'
+        data = np.array([0b10000, 0b110000,
+                         0b11000000000001111,
+                         0b101010011])
+        binda = BinaryDataParser(data, timepatch)
+        times = np.array([1, 3, 2048, 21], dtype=np.uint64)
+        calced = binda._BinaryDataParser__get_time()
+        np.testing.assert_array_equal(calced, times)
+
+    def test_time_with_tp5(self):
+        timepatch = '5'
+        data = np.array([0b10000, 0b110000,
+                         0b1000000000001111,
+                         0b101010011,
+                         0b1111010010110000110100010])
+        binda = BinaryDataParser(data, timepatch)
+        times = np.array([1, 3, 2048, 21, 955930], dtype=np.uint64)
+        calced = binda._BinaryDataParser__get_time()
+        np.testing.assert_array_equal(calced, times)
+
+    def test_sweep_standard(self):
+        timepatch = '5'
+        data = np.array([0b00000000001_01010101010101010101_0101,
+                         0b00000000111_01010101010101010101_0101,
+                         0b10010101_01010101010101010101_0101,
+                         0b11010101_01010101010101010101_0101,
+                         0b00010101_01010101010101010101_0101])
+        binda = BinaryDataParser(data, timepatch)
+        sweeps = np.array([1, 7, 149, 213, 21], dtype=np.uint16)
+        calced = binda._BinaryDataParser__get_sweep()
+        np.testing.assert_array_equal(calced, sweeps)
+
+    def test_tag_standard(self):
+        timepatch = '5b'
+        data = np.array([0b001_0001010101000001_0101101001010101010101010101_0101,
+                         0b100_0001010101000001_0101101001010101010101010101_0101,
+                         0b010101_0001010101000001_0101101001010101010101010101_0101],
+                        dtype=np.uint64)
+        binda = BinaryDataParser(data, timepatch)
+        tag = np.array([1, 4, 21], dtype=np.uint16)
+        calced = binda._BinaryDataParser__get_tag()
+        np.testing.assert_array_equal(calced, tag)
+
+    def test_lost_standard(self):
+        timepatch = '5b'
+        data = np.array([0b1_011010011001001_0001010101000001_0101101001010101010101010101_0101,
+                         0b0_011010011001001_0001010101000001_0101101001010101010101010101_0101,
+                         0b1_011010011001001_0001010101000001_0101101001010101010101010101_0101,
+                         0b1],
+                        dtype=np.uint64)
+        binda = BinaryDataParser(data, timepatch)
+        lost = np.array([1, 0, 1, 0], dtype=np.uint8)
+        calced = binda._BinaryDataParser__get_lost()
+        np.testing.assert_array_equal(calced, lost)
+
