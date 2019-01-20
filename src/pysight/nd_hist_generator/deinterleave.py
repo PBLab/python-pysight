89fa67b71741a61ee6f98c584bfddc3b2669b1aa
HagaiHargil <hagaihargil@gmail.com>
2019-01-20 11:28:36 +0200

    REFACTOR: formatted deinterleave with black, all tests_pass

--- src/pysight/nd_hist_generator/deinterleave.py
+++ src/pysight/nd_hist_generator/deinterleave.py
@@ -18,6 +18,7 @@ class Deinterleave:
     This class should be used when two (or more, in the future) beams
     are co-aligned and excite the sample near-simultaneously.
     """
+
     photons = attr.ib(validator=instance_of(pd.DataFrame))
     reprate = attr.ib(default=80.3e6, validator=instance_of(float))
     binwidth = attr.ib(default=800e-12, validator=instance_of(float))
@@ -36,18 +37,23 @@ class Deinterleave:
         late_photons_mask = self.photons.mask(
             self.photons["time_rel_pulse"] > (self.bins_bet_pulses // 2)
         )
-        early_photons = (late_photons_mask
-            .dropna()
+        early_photons = (
+            late_photons_mask.dropna()
             .assign(time_rel_pulse=lambda x: x.time_rel_pulse.astype(np.uint8))
-            .xs(1, level=0))
-        late_photons = (self.photons
-            .loc[late_photons_mask["time_rel_pulse"].isna(), :]
-            .xs(1, level=0))
+            .xs(1, level=0)
+        )
+        late_photons = self.photons.loc[
+            late_photons_mask["time_rel_pulse"].isna(), :
+        ].xs(1, level=0)
 
         # TODO: Change following paragraph when pandas 0.24 hits with Int64 extension type
         for column in early_photons.columns:
-            early_photons[column] = early_photons[column].astype(late_photons[column].dtype)
+            early_photons[column] = early_photons[column].astype(
+                late_photons[column].dtype
+            )
 
-        new_photons = pd.concat((early_photons, late_photons), keys=[1, 7], names=['Channel'])
+        new_photons = pd.concat(
+            (early_photons, late_photons), keys=[1, 7], names=["Channel"]
+        )
         assert len(new_photons) == len(early_photons) + len(late_photons)
         return new_photons
