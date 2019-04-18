ce71a9b35f979588f998b25bac51f7d46ae98bf3
HagaiHargil <hagaihargil@gmail.com>
2019-04-18 09:43:07 -0700

    FEATURE: added more flim functionality

--- src/pysight/post_processing/flim.py
+++ src/pysight/post_processing/flim.py
@@ -3,6 +3,7 @@ Methods to calculate the lifetime of the generated
 stacks
 """
 from typing import Union
+import logging
 
 import numpy as np
 import scipy.optimize
@@ -10,6 +11,11 @@ import attr
 from attr.validators import instance_of, in_
 
 from pysight.nd_hist_generator.outputs import PySightOutput
+
+
+def in_channel(instance, attribute, value):
+    if value not in instance.data.available_channels:
+        return ValueError
 
 
 @attr.s
@@ -20,24 +26,64 @@ class LifetimeCalc:
     """
 
     data = attr.ib(validator=instance_of(PySightOutput))
-    channel = attr.ib(validator=instance_of(int))
+    channel = attr.ib(validator=[instance_of(int), in_channel])
+    num_of_bins = attr.ib(default=125, validator=instance_of(int))
     raw_arrival_times = attr.ib(init=False)
+    hist = attr.ib(init=False)
+    params = attr.ib(init=False)
+    cov = attr.ib(init=False)
 
-    def __attrs_post_init__(self):
-        assert self.channel in self.data.available_channels
-
-    def fit(self):
+    def fit_entire_fov(self):
         """
         Main method for class
         """
         self.raw_arrival_times = self._get_photons()
+        self.hist = self._generate_aligned_hist(self.raw_arrival_times)
+        try:
+            lowest_bin = self._get_low_bin_idx(self.hist)
+        except ValueError:  # no valid lowest bin
+            logging.warning("No valid decay curve found in the histogram.")
+            return
+        chopped_hist = self.hist[:lowest_bin]
+        self._fit_decay(chopped_hist)
 
     def _get_photons(self):
         """
         Returns an array of the photon arrival times relative to the
         last laser pulse.
         """
-        return self.data.photons.xs("Channel", self.channel).time_rel_pulse.to_numpy()
+        return self.data.photons.xs(
+            self.channel, level="Channel"
+        ).time_rel_pulse.to_numpy()
+
+    def _gen_aligned_hist(self, photons):
+        """ Creates a histogram from the photon list and then moves it so that the highest
+        bin is in the start of it.
+        """
+        hist, bins = np.histogram(photons, bins=self.num_of_bins)
+        hist = np.roll(hist, -hist.argmax())
+        return hist
+
+    def _get_low_bin_idx(self, hist):
+        """
+        Finds in the histogram the edge of the decay curve, i.e.
+        the lowest bin.
+        """
+        lowest = hist.argmin()
+        if lowest < 2:
+            raise ValueError
+        return lowest
+
+    def _get_bin_idx(self):
+        """ Finds the highest and lowest bins in the histogram of the photon arrival times """
+        high_bin = self.hist.argmax()
+
+    def _fit_decay(self, y):
+        """
+        Use a curve fitting method to find the parameters of the
+        exponential decay of the data
+        """
+
 
     @staticmethod
     def single_exp(x, amp, tau, c):
--- tests/test_flim.py
+++ tests/test_flim.py
@@ -11,7 +11,8 @@ def generate_flim():
         if di is None:
             di = {1: np.array([1, 2, 3])}
         if photons is None:
-            photons = pd.DataFrame({'time_rel_pulse': np.array([1, 2, 3])})
+            photons = pd.DataFrame({'time_rel_pulse': np.arange(125),
+            'Frames': 1, 'Channel': 1}).set_index(['Channel', 'Frames'])
         summed_mem = di
         stack = di
         channels = pd.CategoricalIndex([1])
@@ -21,8 +22,24 @@ def generate_flim():
 
 
 class TestFlim:
-    shape = (10, 10, 10)
+    shape = (10, 10, 10, 10)
     flim = True
 
     def test_get_photons(self, generate_flim):
-        np.array_equal(generate_flim(self.shape, self.flim)._get_photons(), np.array([1, 2, 3]))
+        np.array_equal(generate_flim(self.shape, self.flim)._get_photons(), np.arange(125))
+
+    def test_aligned_hist(self, generate_flim):
+        photons = np.arange(31)
+        photons = np.concatenate((photons, [30, 30, 30, 29, 29, 28]))
+        photons_df = pd.DataFrame({'time_rel_pulse': photons, 'Frames': 1, 'Channel': 1}).set_index(['Channel', 'Frames'])
+        hist = generate_flim(self.shape, self.flim, photons=photons_df)._gen_aligned_hist(photons)
+        assert hist.argmax() == 0
+
+    def test_lowest_bin(self, generate_flim):
+        photons = np.linspace(0, 31, num=5, endpoint=False)
+        photons = np.concatenate((photons, [30, 30, 30, 29, 29, 28]))
+        photons = np.delete(photons, 0)
+        photons_df = pd.DataFrame({'time_rel_pulse': photons, 'Frames': 1, 'Channel': 1}).set_index(['Channel', 'Frames'])
+        flim_obj = generate_flim(self.shape, self.flim, photons=photons_df)
+        hist = flim_obj._gen_aligned_hist(photons)
+        assert 2 == flim_obj._get_low_bin_idx(hist)
