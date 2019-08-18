465f864b0ed0ed402c569aaa8ecf6811ad238ce6
HagaiHargil <hagaihargil@gmail.com>
2019-08-18 12:30:33 +0300

    edit some tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,11 +2,13 @@
 __author__ = Hagai Har-Gil
 """
 from unittest import TestCase
-import numpy as np
 import pathlib
-import h5py
 
-from pysight.main import run
+import h5py
+import toml
+import numpy as np
+
+from pysight.main import main_data_readout
 
 
 class TestEntirePipeline(TestCase):
@@ -15,7 +17,9 @@ class TestEntirePipeline(TestCase):
     def test_standard_pipeline(self):
         cfg_file = str(next(pathlib.Path(".").rglob("*jul.toml")).absolute())
         data_file = str(next(pathlib.Path(".").rglob("*jul.hdf5")).absolute())
-        out = run(cfg_file)
+        with open(cfg_file, "r") as f:
+            config: Dict[str, Any] = toml.load(f)
+        out = main_data_readout(config)
         with h5py.File(data_file, "r") as f:
             np.testing.assert_array_equal(
                 out.ch1.time_summed, np.array(f["/Summed Stack/Channel 1"])
@@ -24,7 +28,9 @@ class TestEntirePipeline(TestCase):
     def test_mscan_pipeline(self):
         cfg_file = str(next(pathlib.Path(".").rglob("*fly.toml")).absolute())
         data_file = str(next(pathlib.Path(".").rglob("*fly.hdf5")).absolute())
-        out = run(cfg_file)
+        with open(cfg_file, "r") as f:
+            config: Dict[str, Any] = toml.load(f)
+        out = main_data_readout(config)
         with h5py.File(data_file, "r") as f:
             np.testing.assert_array_equal(
                 out.ch1.time_summed, np.array(f["/Summed Stack/Channel 1"])
@@ -33,7 +39,9 @@ class TestEntirePipeline(TestCase):
     def test_tag_pipeline(self):
         cfg_file = str(next(pathlib.Path(".").rglob("*tag.toml")).absolute())
         data_file = str(next(pathlib.Path(".").rglob("*tag.hdf5")).absolute())
-        out = run(cfg_file)
+        with open(cfg_file, "r") as f:
+            config: Dict[str, Any] = toml.load(f)
+        out = main_data_readout(config)
         with h5py.File(data_file, "r") as f:
             np.testing.assert_array_equal(
                 out.ch1.time_summed, np.array(f["/Summed Stack/Channel 1"])
