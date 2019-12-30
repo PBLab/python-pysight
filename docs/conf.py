4eb303cb957e3ae31d9dbce7e5c77fe154786bc8
HagaiHargil <hagaihargil@gmail.com>
2019-12-30 22:06:06 +0200

    Bump version: 0.11.0 → 0.11.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = "PySight"
 year = "2019"
 author = "Hagai Har-Gil"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.11.0"
+version = release = "0.11.1"
 autodoc = True
 
 pygments_style = "trac"
